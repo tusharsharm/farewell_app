ba627ce1459548f4b950d4f43c9c148bbfd3f150
Tushar Sharma <124043637+tusharsharm@users.noreply.github.com>
2025-03-31 14:02:52 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('dist'));
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
